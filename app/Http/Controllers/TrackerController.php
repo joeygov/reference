bb4b85efcb503c70cc683e6fb5e50870b44d1c59
Shena <shenamarie.dabalos@gmail.com>
2021-01-13 17:08:45 +0800

    [FIX] - namespace

--- app/Http/Controllers/TrackerController.php
+++ app/Http/Controllers/TrackerController.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\User;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Http\Managers\AttendanceManager;
 use App\Models\Account;
 use Illuminate\Http\Request;
