07334176fdccf3f9374529688185735c3a3efbc2
jeffdavid <jdpotrido@student.passerellesnumeriques.org>
2021-01-19 15:59:58 +0800

    [UPDATE] remove comment

--- resources/views/calendar/list.blade.php
+++ resources/views/calendar/list.blade.php
@@ -6,10 +6,6 @@
 <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.5.0/main.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.5.0/main.css"/>
 <link rel="stylesheet" href="{{ asset('assets/admin/admin.css') }}">
-{{-- <link rel="stylesheet" href="{{ asset('assets/vendors/datatables.min.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/vendors/datatables/css/jquery.dataTables.min.css') }}">
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
-<link rel="stylesheet" href="{{ asset('assets/admin/admin.css') }}"> --}}
 @endpush
 
 @section('maincontent')
@@ -32,16 +28,6 @@
 
 @push('js')
 {!! $calendar->script() !!}
-{{-- <script src="{{ asset('assets/vendors/datatables.min.js') }}"></script>
-<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
-<script src="{{ asset('assets/admin/admin.js') }}"></script> --}}
 @endpush
 
 @endsection
